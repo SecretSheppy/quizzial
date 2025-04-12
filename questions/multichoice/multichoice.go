69032f51547151fc6f020d3fbfb12b60f6625aba
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2025-04-12 15:35:43 +0100

    feat: initial multichoice question framework
    registered the question in the questions/register.go file and multichoice implements QPlugin

---
+++ questions/multichoice/multichoice.go
@@ -0,0 +1,47 @@
+package multichoice
+
+import (
+	"github.com/SecretSheppy/quizzial/pkg/qplugins"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+var config = &qplugins.QPluginData{
+	Name: "MultiChoice",
+	TemplatePath: &qplugins.QPluginUserType{
+		Admin: "",
+		User:  "",
+	},
+	JavaScriptPath: &qplugins.QPluginUserType{
+		Admin: "",
+		User:  "",
+	},
+}
+
+type MultiChoice struct {
+	DB *gorm.DB
+}
+
+func (m *MultiChoice) Data() *qplugins.QPluginData {
+	return config
+}
+
+func (m *MultiChoice) Init(db *gorm.DB) {
+	m.DB = db
+}
+
+func (m *MultiChoice) Migrate() {
+
+}
+
+func (m *MultiChoice) SaveQuestionHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (m *MultiChoice) VerifyQuestion(ID int64) bool {
+	return true
+}
+
+func (m *MultiChoice) SaveAnswersHandler(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ questions/register.go
@@ -0,0 +1,20 @@
+package questions
+
+import (
+	"github.com/SecretSheppy/quizzial/pkg/qplugins"
+	"github.com/SecretSheppy/quizzial/questions/multichoice"
+)
+
+// RegisteredPlugins is the array of all questions registered in the system. A hardcoded approach was chosen over the
+// plugin package due to its lack of support and severe limitations.
+var registeredQuestions = []qplugins.QPlugin{
+	&multichoice.MultiChoice{},
+}
+
+func GetAllQuestions() map[string]qplugins.QPlugin {
+	var questions map[string]qplugins.QPlugin
+	for _, plugin := range registeredQuestions {
+		questions[plugin.Data().Name] = plugin
+	}
+	return questions
+}
