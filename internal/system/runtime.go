65c4426ef132bda25f735ef7b7124bb08e260c52
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2025-04-12 23:57:34 +0100

    feat: added initial runtime logic

--- internal/system/runtime.go
+++ internal/system/runtime.go
@@ -1,7 +1,25 @@
 package system
 
-import "fmt"
+import (
+	"github.com/SecretSheppy/quizzial/internal/sdb"
+	"github.com/SecretSheppy/quizzial/questions"
+	"github.com/joho/godotenv"
+)
 
 func Runtime() {
-	fmt.Println("Quizzial")
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	DB := sdb.Get()
+	qs := questions.All()
+
+	for _, q := range qs {
+		q.Init(DB)
+
+		if err = q.Migrate(); err != nil {
+			panic(err)
+		}
+	}
 }
