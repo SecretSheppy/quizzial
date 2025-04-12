4d612aaf6c7f2b46267b527763fca2da5f36e57b
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2025-04-12 19:55:18 +0100

    feat: system runtime framework

---
+++ cmd/quizzial/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/SecretSheppy/quizzial/internal/system"
+)
+
+func main() {
+	system.Runtime()
+}
---
+++ internal/system/runtime.go
@@ -0,0 +1,7 @@
+package system
+
+import "fmt"
+
+func Runtime() {
+	fmt.Println("Quizzial")
+}
