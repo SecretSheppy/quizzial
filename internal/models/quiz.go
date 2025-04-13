34208680c620cb465d4b47d629b2c1f7044937ae
SecretSheppy <62794249+SecretSheppy@users.noreply.github.com>
2025-04-13 02:05:49 +0100

    test(models): created tests for NewQuiz

--- internal/models/quiz.go
+++ internal/models/quiz.go
@@ -1,11 +1,37 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
 
 type Quiz struct {
 	QuizID       uuid.UUID `gorm:"primaryKey"`
-	QuizMasterID uuid.UUID
-	ShortID      string `gorm:"unique"`
-	Title        string
+	QuizMasterID uuid.UUID `gorm:"not null"`
+	ShortID      string    `gorm:"not null;unique"`
+	Title        string    `gorm:"not null"`
 	Sections     []Section `gorm:"foreignkey:QuizID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
+func NewQuiz(qmID uuid.UUID, shortID, title string) (*Quiz, error) {
+	if qmID == uuid.Nil {
+		return nil, fmt.Errorf("qmID cannot be nil")
+	}
+
+	if shortID == "" {
+		return nil, fmt.Errorf("shortID cannot be nil")
+	}
+
+	if title == "" {
+		return nil, fmt.Errorf("title cannot be nil")
+	}
+
+	q := &Quiz{
+		QuizID:       uuid.New(),
+		QuizMasterID: qmID,
+		ShortID:      shortID,
+		Title:        title,
+	}
+
+	return q, nil
 }
---
+++ internal/models/quiz_test.go
@@ -0,0 +1,89 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"testing"
+)
+
+func TestNewQuiz(t *testing.T) {
+	db, err := setup()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = db.AutoMigrate(&QuizMaster{}, &Quiz{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		err = teardown(db)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	frodo, err := NewQuizMaster("Frodo Bagins", "1234")
+	if err != nil {
+		t.Errorf("failed to create Frodo Bagins: %v", err)
+	}
+
+	result := db.Debug().Create(&frodo)
+	if result.Error != nil {
+		t.Error(result.Error)
+	}
+
+	t.Run("Create a new quiz", func(t *testing.T) {
+		q, err := NewQuiz(frodo.QuizMasterID, "XX-XX-XX", "The Lord of the Rings")
+		if err != nil {
+			t.Errorf("failed to create a new quiz: %v", err)
+		}
+
+		result := db.Debug().Create(&q)
+		if result.Error != nil {
+			t.Error(result.Error)
+		}
+
+		db.Preload("Quizzes").First(frodo, frodo.QuizMasterID)
+		if len(frodo.Quizzes) == 0 {
+			t.Errorf("quiz master Frodo Bagins has no quizzes")
+		}
+
+		if frodo.Quizzes[0].Title != "The Lord of the Rings" {
+			t.Errorf("quiz created with incorrect title")
+		}
+	})
+
+	t.Run("Create a new quiz without a quiz master ID", func(t *testing.T) {
+		_, err := NewQuiz(uuid.Nil, "XX-XX-XX", "The Lord of the Rings")
+		if err == nil {
+			t.Errorf("unexpected success creating a new quiz without a quiz master ID")
+		}
+	})
+
+	t.Run("Create a new quiz with a short ID", func(t *testing.T) {
+		_, err := NewQuiz(uuid.Nil, "", "The Lord of the Rings")
+		if err == nil {
+			t.Errorf("unexpected success creating a new quiz with a short ID")
+		}
+	})
+
+	t.Run("Create a new quiz without a title", func(t *testing.T) {
+		_, err := NewQuiz(uuid.Nil, "XX-XX-XX", "")
+		if err == nil {
+			t.Errorf("unexpected success creating a new quiz without a title")
+		}
+	})
+
+	t.Run("Delete a quiz", func(t *testing.T) {
+		result := db.Delete(frodo.Quizzes[0])
+		if result.Error != nil {
+			t.Errorf("failed to delete quiz: %v", result.Error)
+		}
+
+		db.Preload("Quizzes").First(frodo, frodo.QuizMasterID)
+		if len(frodo.Quizzes) != 0 {
+			t.Errorf("quiz master Frodo Bagins has quizzes, but should have none")
+		}
+	})
+}
